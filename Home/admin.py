8817e16b66be38629e0e7477d5f0430c37371c6b
Johnny <urdin32@gmail.com>
2020-11-17 11:38:12 -0500

    Configuración del proyecto

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from Home.models import Usuario, Direccion
+from eraly2.snippers import Attr
+
+
+@admin.register(Usuario)
+class AdminUsuario(admin.ModelAdmin):
+    list_display = Attr(Usuario)
+    list_display_links = Attr(Usuario)
+
+
+@admin.register(Direccion)
+class AdminDirecciones(admin.ModelAdmin):
+    list_display = Attr(Direccion)
+    list_display_links = Attr(Direccion)
