85ff8923ce001a4ec441f1dc07ca56e918071770
Johnny <urdin32@gmail.com>
2020-11-17 17:32:38 -0500

    Proyecto diseño Template - Johnny Urdin

--- Home/views.py
+++ Home/views.py
@@ -1,10 +1,13 @@
+
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     if request.user.is_authenticated:
+
         return render(request, "Home/index2.html")
     else:
         return render(request, "Home/login.html")
