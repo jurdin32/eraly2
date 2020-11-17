f176efb679ac8e019657acdbbe784572ed6507a7
Johnny <urdin32@gmail.com>
2020-11-17 11:31:53 -0500

    Configuración del proyecto

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
