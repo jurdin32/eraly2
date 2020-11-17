6ebff27c178f3443a18ddc370b804c6ab090c8c8
Johnny <urdin32@gmail.com>
2020-11-17 17:16:48 -0500

    Proyecto diseño Template - Johnny Urdin

--- eraly2/urls.py
+++ eraly2/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path("",index),
+    path("logout_/",logout_),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
