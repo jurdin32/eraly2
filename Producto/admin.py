aa5c3116038c22b1befee33c79a58c1d28159ac0
Johnny <urdin32@gmail.com>
2020-11-17 19:18:33 -0500

    Proyecto diseño Template - Johnny Urdin

--- Producto/admin.py
+++ Producto/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from Producto.models import Proveedor
+from eraly2.snippers import Attr
+
+
+@admin.register(Proveedor)
+class AdminProveedor(admin.ModelAdmin):
+    list_display = Attr(Proveedor)
+    list_display_links = Attr(Proveedor)
--- Producto/models.py
+++ Producto/models.py
@@ -7,5 +7,4 @@ from Home.models import Ciudad
 class Proveedor(models.Model):
     ruc=models.CharField(max_length=10)
     nombreComercial=models.CharField(max_length=60)
-    ciudad=models.ForeignKey(Ciudad,on_delete=models.CASCADE)
-    direccion=models.CharField(max_length=60)
+    ciudad=models.ForeignKey(Ciudad,on_delete=models.CASCADE,null=True,blank=True)
