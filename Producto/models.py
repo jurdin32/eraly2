92bef8137e3838283b5b4d3b648437b18b202562
Johnny <urdin32@gmail.com>
2020-11-17 19:19:49 -0500

    Proyecto diseño Template - Johnny Urdin

--- Producto/models.py
+++ Producto/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-from Home.models import Ciudad
+from Home.models import Ciudad, Direccion
 
 
 class Proveedor(models.Model):
     ruc=models.CharField(max_length=10)
     nombreComercial=models.CharField(max_length=60)
     ciudad=models.ForeignKey(Ciudad,on_delete=models.CASCADE,null=True,blank=True)
+    direccion=models.ForeignKey(Direccion,on_delete=models.CASCADE,null=True,blank=True)
