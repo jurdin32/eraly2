bc4b5d3a090c2ba5feec98bf0659bbf1375cb6ca
Johnny <urdin32@gmail.com>
2020-11-24 10:44:43 -0500

    Empresa - Johnny Urdin - 24/11

--- Empresa/views.py
+++ Empresa/views.py
@@ -5,13 +5,15 @@ from Empresa.models import Establecimiento
 
 
 def empresa(request):
+    if request.POST:
+        print(request.POST)
+        establecimiento=Establecimiento(usuario=request.user,ruc=request.POST['ruc'],
+                        representateLegal=request.POST['representateLegal'],
+                        nombreComercial=request.POST['nombreComercial'],
+                        descripcion=request.POST['descripcion']
+                        )
+        establecimiento.save()
     contexto={
         "empresas":Establecimiento.objects.filter(usuario=request.user)
     }
     return render(request, "empresa/empresa.html", contexto)
-
-def registroEmpresa(request):
-    contexto={
-
-    }
-    return render(request,'empresa/empresa.html',contexto)
