347bc56697315e10e11dd774d03c30c021c0528a
Manuel Benitez <mabg.dg@gmail.com>
2020-11-24 21:52:19 -0500

    Manuel - Cambios Iniciales 4

--- Producto/admin.py
+++ Producto/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from Producto.models import Proveedor, DireccionProveedor
+from Producto.models import Proveedor, DireccionProveedor, Categorias, Subcategorias, DetallesProducto, Productos, \
+    Colores, Precios, Kardex, ImagenesProducto
 from eraly2.snippers import Attr
 
 class DireccionInline(admin.StackedInline):
@@ -14,6 +15,45 @@ class AdminProveedor(admin.ModelAdmin):
     list_display_links = Attr(Proveedor)
     inlines = [DireccionInline]
 
+@admin.register(Categorias)
+class Categorias(admin.ModelAdmin):
+    list_display = Attr(Categorias)
+    list_display_links = Attr(Categorias)
+
+@admin.register(Subcategorias)
+class Subcategorias(admin.ModelAdmin):
+    list_display = Attr(Subcategorias)
+    list_display_links = Attr(Subcategorias)
+
+@admin.register(DetallesProducto)
+class DetallesProducto(admin.ModelAdmin):
+    list_display = Attr(DetallesProducto)
+    list_display_links = Attr(DetallesProducto)
+
+@admin.register(Productos)
+class Productos(admin.ModelAdmin):
+    list_display = Attr(Productos)
+    list_display_links = Attr(Productos)
+
+@admin.register(Colores)
+class Colores(admin.ModelAdmin):
+    list_display = Attr(Colores)
+    list_display_links = Attr(Colores)
+
+@admin.register(Precios)
+class Precios(admin.ModelAdmin):
+    list_display = Attr(Precios)
+    list_display_links = Attr(Precios)
+
+@admin.register(Kardex)
+class Kardex(admin.ModelAdmin):
+    list_display = Attr(Kardex)
+    list_display_links = Attr(Kardex)
+
+@admin.register(ImagenesProducto)
+class ImagenesProducto(admin.ModelAdmin):
+    list_display = Attr(ImagenesProducto)
+    list_display_links = Attr(ImagenesProducto)
 
 @admin.register(DireccionProveedor)
 class AdminDireccion(admin.ModelAdmin):
--- Producto/models.py
+++ Producto/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
 from Home.models import Ciudad
 
 class Proveedor(models.Model):
@@ -12,3 +14,101 @@ class DireccionProveedor(models.Model):
     ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
     direccion=models.CharField(max_length=60)
     telefono=models.CharField(max_length=10)
+
+class Categorias(models.Model):
+    icono=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=100)
+
+    def __str__(self):
+        return '%s' % (self.nombre)
+
+    class Meta:
+        verbose_name_plural = "1. Categorias "
+
+
+class Subcategorias(models.Model):
+    icono=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=100)
+    categoria_id=models.ForeignKey(Categorias, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % (self.categoria_id)
+
+    class Meta:
+        verbose_name_plural = "2. Subcategorias "
+
+class DetallesProducto(models.Model):
+    icono=models.CharField(max_length=10)
+    nombre=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=100)
+    categoria_id=models.ForeignKey(Categorias, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s | %s'%(self.nombre, self.categoria_id)
+
+
+    class Meta:
+        verbose_name_plural = "3. Detalles del Producto "
+
+class Productos(models.Model):
+    subcategoria_id=models.ForeignKey(Subcategorias, on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=100)
+    talla=models.CharField(max_length=10)
+    dimension=models.CharField(max_length=50)
+    descripcion=models.CharField(max_length=100)
+    estado=models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s'%(self.nombre)
+
+    class Meta:
+        verbose_name_plural = "4. Producto "
+
+class Colores(models.Model):
+    producto_id=models.ForeignKey(Productos, on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=50)
+    codigoColor=models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s'%(self.nombre)
+
+    class Meta:
+        verbose_name_plural = "5. Color Producto "
+
+class Precios(models.Model):
+    producto_id=models.ForeignKey(Productos, on_delete=models.CASCADE)
+    precioVenta=models.DecimalField(max_digits=5, decimal_places=2)
+    detalle=models.CharField(max_length=50)
+
+
+    class Meta:
+        verbose_name_plural = "6. Color Producto "
+
+class Kardex(models.Model):
+    producto_id=models.ForeignKey(Productos, on_delete=models.CASCADE)
+    tipo=models.CharField(max_length=50)
+    fecha=models.DateTimeField(auto_now = True)
+    cantidad=models.IntegerField(default=0)
+    descripcion=models.CharField(max_length=150)
+
+
+    class Meta:
+        verbose_name_plural = "7. Kardex "
+
+class ImagenesProducto(models.Model):
+    producto_id=models.ForeignKey(Productos, on_delete=models.CASCADE)
+    imagen=models.ImageField(upload_to='producto', null=True, blank=True, help_text='100x100')
+
+    def __str__(self):
+        return '%s' % (self.producto_id)
+
+    def miniatura(self):
+        return mark_safe("<img src='/media/%s' style='width: 100px'>" % self.imagen)
+
+    class Meta:
+        verbose_name_plural = "8. Imagenes de Producto "
+
+
+
