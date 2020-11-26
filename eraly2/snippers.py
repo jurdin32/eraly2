14a846b02f314a9002009ed70bcfe30f3ac72f2d
Johnny <urdin32@gmail.com>
2020-11-25 19:23:01 -0500

    Empresa - Johnny Urdin - 25/11

--- eraly2/snippers.py
+++ eraly2/snippers.py
@@ -9,11 +9,8 @@ def Attr(cls):
     model = cls.__name__
     return cls.__doc__.replace(model, "").replace("(", "").replace(")", "").replace(" ", "").split(",")
 
-def get_filename(filename):
-    return filename.upper()
-
-
 def Hash_parse(text):
     h = hashlib.new("sha256", b"%s"%text)
     print(h.digest())
     return h
+
