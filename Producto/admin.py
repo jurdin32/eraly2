2dcdcdf809d28ccf15eaa7626eedb96247154a04
Johnny <urdin32@gmail.com>
2020-11-26 08:58:02 -0500

    Empresa - Johnny Urdin - 26/11

--- Producto/admin.py
+++ Producto/admin.py
@@ -9,6 +9,7 @@ class AdminTipoProveedor(admin.ModelAdmin):
     list_display = Attr(TipoProveedor)
     list_display_links = Attr(TipoProveedor)
 
+
 @admin.register(ActividadProveedor)
 class AdminActividadProveedor(admin.ModelAdmin):
     list_display = Attr(ActividadProveedor)
@@ -28,42 +29,42 @@ class AdminProveedor(admin.ModelAdmin):
 
 
 @admin.register(Categorias)
-class Categorias(admin.ModelAdmin):
+class AdminCategorias(admin.ModelAdmin):
     list_display = Attr(Categorias)
     list_display_links = Attr(Categorias)
 
 @admin.register(Subcategorias)
-class Subcategorias(admin.ModelAdmin):
+class AdminSubcategorias(admin.ModelAdmin):
     list_display = Attr(Subcategorias)
     list_display_links = Attr(Subcategorias)
 
 @admin.register(DetallesProducto)
-class DetallesProducto(admin.ModelAdmin):
+class AdminDetallesProducto(admin.ModelAdmin):
     list_display = Attr(DetallesProducto)
     list_display_links = Attr(DetallesProducto)
 
 @admin.register(Productos)
-class Productos(admin.ModelAdmin):
+class AdminProductos(admin.ModelAdmin):
     list_display = Attr(Productos)
     list_display_links = Attr(Productos)
 
 @admin.register(Colores)
-class Colores(admin.ModelAdmin):
+class AdminColores(admin.ModelAdmin):
     list_display = Attr(Colores)
     list_display_links = Attr(Colores)
 
 @admin.register(Precios)
-class Precios(admin.ModelAdmin):
+class AdminPrecios(admin.ModelAdmin):
     list_display = Attr(Precios)
     list_display_links = Attr(Precios)
 
 @admin.register(Kardex)
-class Kardex(admin.ModelAdmin):
+class AdminKardex(admin.ModelAdmin):
     list_display = Attr(Kardex)
     list_display_links = Attr(Kardex)
 
 @admin.register(ImagenesProducto)
-class ImagenesProducto(admin.ModelAdmin):
+class AdminImagenesProducto(admin.ModelAdmin):
     list_display = Attr(ImagenesProducto)
     list_display_links = Attr(ImagenesProducto)
 
