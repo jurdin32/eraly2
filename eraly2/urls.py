662b53635da0e14b89a24371f0c5bffde9a4880b
Johnny <urdin32@gmail.com>
2020-11-30 15:20:46 -0500

    Empresa - Johnny Urdin - 30/11

--- eraly2/urls.py
+++ eraly2/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from Empresa.views import empresa, eliminar_empresa, direcciones, modificar_empresa, crear_direcciones, \
-    modificar_direcciones
+from Empresa.views import *
 from Home.views import *
 from Producto.views import productos, categorias, proveedores, kardex
 
