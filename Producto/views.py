0b03aab8a08007a643c8e7a988557a7ec4a7f55b
Johnny <urdin32@gmail.com>
2020-12-07 10:27:38 -0500

    Direcciones - Johnny Urdin - 07/12

--- Producto/views.py
+++ Producto/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # Create your views here.
+from Empresa.models import Establecimiento
+from Producto.models import Proveedor
+
+
 def proveedores(request):
     contexto={
+        'establecimientos': Establecimiento.objects.filter(usuario=request.user),
+        'proveedores': Proveedor.objects.filter(establecimiento__usuario=request.user)
 
     }
     return render(request, "producto/proveedores.html",contexto)
