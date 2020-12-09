8d1d9357155f37eba96666497bb64518f0896327
Johnny <urdin32@gmail.com>
2020-12-09 11:41:58 -0500

    Proveedores - Johnny Urdin - 09/12

--- Producto/views.py
+++ Producto/views.py
@@ -5,9 +5,27 @@ from Producto.models import Proveedor
 
 
 def proveedores(request):
+    mensaje=""
+    tipo=""
+    if request.POST:
+        try:
+            if request.FILES:
+                Proveedor.objects.create(establecimiento_id=request.POST['establecimiento'],logo=request.FILES['logo'], ruc=request.POST['ruc'],nombre_fantasia=request.POST['nombreComercial'],
+                                         representante=request.POST['representateLegal']).save()
+            else:
+                Proveedor.objects.create(establecimiento_id=request.POST['establecimiento'],
+                                         ruc=request.POST['ruc'], nombre_fantasia=request.POST['nombreComercial'],
+                                         representante=request.POST['representateLegal']).save()
+            mensaje = "El registro se ha creado exitosamente..! "
+            tipo = "success"
+        except Exception as error:
+            mensaje="Al parecer ocurrio un error: "+str(error)
+            tipo="danger"
     contexto={
         'establecimientos': Establecimiento.objects.filter(usuario=request.user),
-        'proveedores': Proveedor.objects.filter(establecimiento__usuario=request.user)
+        'proveedores': Proveedor.objects.filter(establecimiento__usuario=request.user),
+        'mensaje':mensaje,
+        'tipo':tipo,
 
     }
     return render(request, "producto/proveedores.html",contexto)
