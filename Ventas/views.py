1a8bff2c1196c3329d95c69b777ff449a3898e0a
Johnny <urdin32@gmail.com>
2020-12-30 17:00:39 -0500

    Productos - Productos 30/12

--- Ventas/views.py
+++ Ventas/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from Empresa.models import Establecimiento
+from Producto.models import Productos
 
 
 def proformas(request,id=0):
     establecimiento=None
     establecimientos=None
+    productos=None
     if int(id) > 0:
         establecimiento = Establecimiento.objects.get(id=id)
     else:
         establecimientos = Establecimiento.objects.filter(usuario=request.user)
+        productos=Productos.objects.filter(establecimiento_id=id)
+
 
     contexto={
         'establecimiento':establecimiento,
-        'establecimientos':establecimientos
+        'establecimientos':establecimientos,
+        'productos':productos,
     }
     return render(request, 'Ventas/proformas.html', contexto)
