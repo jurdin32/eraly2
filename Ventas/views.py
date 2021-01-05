15788e23345c63c51939755b415e8ff847cb2151
Johnny <urdin32@gmail.com>
2021-01-05 10:53:56 -0500

    Productos - Productos 02/01/2021

--- Ventas/views.py
+++ Ventas/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from Empresa.models import Establecimiento
+from Personas.models import Clientes
 from Producto.models import Productos
 
 
@@ -16,6 +17,7 @@ def proformas(request,id=0):
     if int(id) > 0:
         establecimiento = Establecimiento.objects.get(id=id)
         productos = Productos.objects.filter(establecimiento_id=id)
+        clientes=Clientes.objects.filter(establecimiento_id=id)
     else:
         establecimientos = Establecimiento.objects.filter(usuario=request.user)
         contador=establecimientos.count()
@@ -26,5 +28,6 @@ def proformas(request,id=0):
         'establecimiento':establecimiento,
         'establecimientos':establecimientos,
         'productos':productos,
+        'clientes':clientes
     }
     return render(request, 'Ventas/proformas.html', contexto)
