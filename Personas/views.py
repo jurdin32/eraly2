92393c416551bbf722db779c45a0dc5599db8031
Johnny <urdin32@gmail.com>
2021-01-27 15:44:47 -0500

    Errores 27/01/2021

--- Personas/views.py
+++ Personas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Personas.models import Clientes
+
+
+def registroCLientes(request):
+    contexto={
+        'clientes':Clientes.objects.filter(establecimiento__usuario=request.user)
+    }
+    return render(request, 'personas/clientes.html',contexto)
