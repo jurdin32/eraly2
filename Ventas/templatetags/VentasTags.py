918da7d70ee32f707da25010bb811eb5d2563832
Johnny <urdin32@gmail.com>
2021-02-07 11:20:17 -0500

    proforma 07/02/2021

---
+++ Ventas/templatetags/VentasTags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.db.models import Sum
+
+from Producto.models import Productos, Kardex, Precios
+from Ventas.models import Recibos, CuentasCobrar
+
+register = template.Library()
+
+@register.simple_tag
+def pagos(id_cuenta):
+    xpagos=0
+    try:
+        xpagos=Recibos.objects.filter(cuenta_id=id_cuenta).aggregate(Sum('cantidad'))['cantidad__sum']
+    except:
+        xpagos =0
+    return xpagos
+
+@register.simple_tag
+def saldos(id_cuenta):
+    cuenta=CuentasCobrar.objects.get(id=id_cuenta).cantidad
+    xpagos=0
+    try:
+        xpagos=Recibos.objects.filter(cuenta_id=id_cuenta).aggregate(Sum('cantidad'))['cantidad__sum']
+    except:
+        xpagos =0
+    return float(cuenta) - float(xpagos)
