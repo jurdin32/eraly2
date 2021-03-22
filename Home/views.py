48e611982650e2a423835b92938dbd0b6395dff0
Johnny <urdin32@gmail.com>
2021-03-22 09:50:09 -0500

    Cambios en categoria y proveedores, se podrá seleccionar el establecimiento  -  19/03/2021

--- Home/views.py
+++ Home/views.py
@@ -19,6 +19,8 @@ def index(request):
             if user is not None and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect("/")
+            else:
+                return render(request, "Home/login.html")
         else:
             return render(request, "Home/login.html")
 
