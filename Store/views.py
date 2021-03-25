5f227e0f2eb6721afaf785c23db9719d43d0ea3e
Johnny <urdin32@gmail.com>
2021-03-25 16:17:11 -0500

    Cambios en la interfaz de la tienda.  -  25/03/2021

--- Store/views.py
+++ Store/views.py
@@ -26,7 +26,7 @@ def _detalles(request):
     fecha = datetime.datetime.now().date()
     producto = Productos.objects.get(hash=request.GET.get('hash'))
     promo =Promociones.objects.filter(precio__producto=producto).last()
-    if promo:
+    if promo!=None:
         print("Fecha inicio",promo.fechaInicio)
         print('Fecha final',promo.fechaFinal)
         print('fecha actual',fecha)
