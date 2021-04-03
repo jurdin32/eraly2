191e76a348166ec4a92c99ec6c01f88673bfc353
Johnny <urdin32@gmail.com>
2021-04-03 09:17:37 -0500

    Cambios en la interfaz de la tienda.  -  03/04/2021

--- Store/views.py
+++ Store/views.py
@@ -325,7 +325,7 @@ def ver_subcategorias(request):
         'max':max,
         'categoria':cat,
         'excento':excento,
-        'colores': Colores.objects.all().distinct()
+        'colores': Colores.objects.all().distinct('codigoColor')
     }
 
     if request.GET.get('grid'):
