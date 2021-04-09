e4d6f0345bd5eac24d4a7ff03ae505cf736bdef6
Johnny <urdin32@gmail.com>
2021-04-08 23:55:47 -0500

    Cambios en la interfaz de la tienda.  -  08/04/2021

--- Store/views.py
+++ Store/views.py
@@ -333,7 +333,7 @@ def ver_subcategorias(request):
 
     if request.GET.get("q"):
         q=request.GET.get("q")
-        prod = prod.filter(nombre__icontains=request.GET.get("q")) or prod.filter(subcategoria__nombre__icontains=request.GET.get("q")) or prod.filter(etiquetas__icontains=request.GET.get("q")) or prod.filter(marca__nombre__icontains=request.GET.get("q")) or prod.filter(establecimiento__nombreComercial__icontains=request.GET.get("q"))
+        prod = prod.filter(nombre__icontains=request.GET.get("q")) or prod.filter(subcategoria__nombre__icontains=request.GET.get("q")) or prod.filter(etiquetas__icontains=request.GET.get("q")) or prod.filter(marca__nombre__icontains=request.GET.get("q")) or prod.filter(establecimiento__nombreComercial__icontains=request.GET.get("q")) or prod.filter(descripcion__icontains=request.GET.get("q")) or prod.filter(detallesTecnicos__icontains=request.GET.get("q"))
     if request.GET.get("ord"):
         if request.GET.get("ord") == "name":
             prod = prod.order_by('nombre')
