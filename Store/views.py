630b410489d18e1ce76433f07a63bd0c97787f62
Johnny <urdin32@gmail.com>
2021-04-11 12:21:14 -0500

    Cambios en la interfaz de la tienda.  -  11/04/2021

--- Store/views.py
+++ Store/views.py
@@ -20,7 +20,8 @@ from eraly2.snippers import Hash_parse
 def tienda(request):
     productos=Productos.objects.filter(precios__web=True)
     puntuados = productos.filter(puntuacion__range=(2, 5)).order_by('-puntuacion')
-    del request.session['tiendas']
+    if request.session['tiendas']:
+        del request.session['tiendas']
     request.session['tiendas']=[{"id":tienda.id,"nombre":tienda.nombreComercial,'slug':tienda.slug} for tienda in Establecimiento.objects.all().order_by('slug')]
     print(request.session['tiendas'])
     paginator = Paginator(puntuados, 20)
