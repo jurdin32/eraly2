77300344558ffa54debd5557e1597a3fa7a160d5
Johnny <urdin32@gmail.com>
2021-04-13 11:49:36 -0500

    Cambios en la interfaz de la tienda.  -  11/04/2021

--- Producto/views.py
+++ Producto/views.py
@@ -364,7 +364,7 @@ def subir_imagenes_producto(request,id):
     return HttpResponse("ok")
 
 def promociones(request):
-    promo=Promociones.objects.filter(precio__producto__establecimiento__usuario=request.user)
+    promo=Promociones.objects.filter(precio__producto__establecimiento__usuarioempresa__user=request.user)
     produc=Productos.objects.filter(establecimiento__usuario=request.user)
     if request.GET.get('establecimiento'):
         promo=Promociones.objects.filter(precio__producto__establecimiento_id= request.GET.get('establecimiento'))
